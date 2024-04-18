924fc9d8d51ecf62c38307901e841438083ce8a3
Thiagolvl9999 <164432518+Thiagolvl9999@users.noreply.github.com>
2024-04-17 21:44:20 -0300

    Add files via upload
    login god

---
+++ ProyectoX/login.php
@@ -0,0 +1,14 @@
+<?php
+$Username="Yatito";
+$Password="Feluelcapo";
+echo "GG Papu";
+
+if(isset($_POST[]))
+
+if (isset($_POST['Ingresar'])) 
+    echo "Hacer validacion";
+    else 
+        echo "Eliminar sesion";
+    
+
+?>
